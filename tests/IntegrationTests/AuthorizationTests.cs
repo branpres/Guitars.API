1776f6e35a5333de00c0f4dd9c3801cc723bcaa9
Brandon Preston <brandon.preston@gmail.com>
2021-11-05 17:18:42 -0400

    Added authorization tests.

--- tests/IntegrationTests/AuthorizationTests.cs
+++ tests/IntegrationTests/AuthorizationTests.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Application.Features.Guitars.Commands.CreateGuitar;
+using Domain.Enums;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using System.Net;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace IntegrationTests
 {
-    public class AuthorizationTests
+    public class AuthorizationTests : TestBase
     {
+        [Test]
+        public async Task UserWithReadPolicyShouldBeAuthorizedToAccessResource()
+        {
+            // Arrange
+            var client = await GetHttpClientForReadOnlyUserAsync();
+
+            // Act
+            var response = await client.GetAsync("/guitars/");
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Test]
+        public async Task UserWithReadPolicyShouldBeForbiddenFromResource()
+        {
+            // Arrange
+            var client = await GetHttpClientForReadOnlyUserAsync();
+
+            var createGuitarCommand = new CreateGuitarCommand
+            {
+                GuitarType = GuitarType.AcousticElectric,
+                MaxNumberOfStrings = 6,
+                Make = "Taylor",
+                Model = "314-CE"
+            };
+
+            // Act
+            var response = await client.PostAsJsonAsync("/guitars", createGuitarCommand);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Forbidden, response.StatusCode);
+        }
+
+        [Test]
+        public async Task AccessToResourceShouldBeUnauthorized()
+        {
+            // Arrange
+            var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/guitars/");
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
     }
 }
--- tests/IntegrationTests/TestBase.cs
+++ tests/IntegrationTests/TestBase.cs
@@ -33,5 +33,21 @@ namespace IntegrationTests
 
             return client;
         }
+
+        public static async Task<HttpClient> GetHttpClientForReadOnlyUserAsync()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            var client = factory.CreateClient();
+
+            // using admin because it doesn't matter who is logged in as authentication is not what we're testing here
+            // although this by nature does, in fact, test logging in as admin
+            var loginCommand = new LoginCommand { UserName = "readonlyuser", Password = "guitarsReadonlyuser1!" };
+            var response = await client.PostAsJsonAsync("/authentication/login", loginCommand);
+            var jwt = await response.Content.ReadFromJsonAsync<string>();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
+
+            return client;
+        }
     }
 }
